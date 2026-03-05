485d03dc76ce5bfa0ba247f1185f22f85a71afe7
pr-estt <mcadamsrd@gmail.com>
2026-03-05 07:15:17 +0000

    refactor our services

--- components/landing/stats.tsx
+++ components/landing/stats.tsx
@@ -26,7 +26,6 @@ const services = [
 export default function Stats() {
   return (
     <section className="py-16 bg-gray-900 px-10 lg:p-20 lg:px-40">
-     <h1 className="text-center text-3xl font-extrabold mb-6">Our Services</h1>
 
      <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
         {services.map((service, index) => (
